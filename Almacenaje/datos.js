8426d1e5b7bce826561bee922dce9dcb47da870b
Xavi Miro <xmiroc@uoc.edu>
2025-11-07 16:07:35 +0100

    feat: archivo almacena - función API interna CRUD y autentificación

---
+++ Almacenaje/datos.js
@@ -0,0 +1,31 @@
+export const USUARIOS = [
+  {
+    nombre: "Admin",
+    email: "admin@ejemplo.com",
+    contraseña: "admin",
+    rol: "admin",
+  },
+  {
+    nombre: "Usuario",
+    email: "usuario@ejemplo.com",
+    contraseña: "1234",
+    rol: "usuario",
+  },
+];
+
+export const VOLUNTARIADOS = [
+  {
+    titulo: "Clases de repaso de mates",
+    email: "user1@correo.com",
+    fecha: "2025-11-21",
+    descripcion: "Ofrezco clases de matemáticas para ESO",
+    tipo: "oferta",
+  },
+  {
+    titulo: "Acompañamiento personas mayores",
+    email: "admin@admin.com",
+    fecha: "2025-11-15",
+    descripcion: "Busco voluntario para visitas a residencia",
+    tipo: "petición",
+  },
+];
